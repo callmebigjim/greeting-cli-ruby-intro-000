2c03e52027151230fda808361ebbe6811c2c742c
Russ Robinson <admin@hostinghelp.co>
2019-12-13 22:26:41 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,9 @@
 def greeting(name)
   puts "Hello #{name}! It's nice to meet you."
+<<<<<<< HEAD
   #puts "Please input your name:"
+=======
+  puts "Please input your name:"
+  name = gets
+>>>>>>> 7c73d900bbc7b6bcf3a8ef46efdfb979842f225f
 end
